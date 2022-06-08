2e55b5cec3bfb15d0fe6cadf32ad9b6ebb501f81
Mateus <mateus.andrade.oliveira08@aluno.ifce.edu.br>
2022-06-08 11:50:59 -0300

    problema da varredura resolvido

--- [14]codigo_fonte_216.py
+++ [14]codigo_fonte_216.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 as cv
 
-f = cv.imread('images/limiar.png', cv.IMREAD_UNCHANGED)
+f = cv.imread('images/limiar.png', cv.IMREAD_GRAYSCALE)
 
 threshold = 150
 
@@ -9,14 +9,14 @@ shape_f = np.shape(f)
 g = np.zeros(shape_f)
 for j in range(0, shape_f[1]):
     for i in range(0, shape_f[0]):
-        if(f[i,j]<150):
+        if(f[i,j]<80):
             g[i,j] = 0
         else:
             g[i,j] = 255
 
-cv.imwrite('output/limiarizacao_4.png', g)
+cv.imwrite('output/limiarizacao_1.png', g)
 
 
-#g = (f >= threshold)*255
+g = (f >= threshold)*255
 
-#cv.imwrite('output/limiarizacao_2.png', g)
+cv.imwrite('output/limiarizacao_x.png', g)
